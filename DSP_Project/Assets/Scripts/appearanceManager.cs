54357c41ba55f14102d1e2147a8bd83d79e203d0
ocesiwm <oceaneperetti@gmail.com>
2019-11-19 10:55:37 +0000

    fixing revert changes features & experimenting with rabbit display

---
+++ DSP_Project/Assets/Scripts/appearanceManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appearanceManager : MonoBehaviour
+{
+	
+	[SerializeField]
+	private Material[] eyesColor;
+	[SerializeField]
+	private Material[] bodyColor;
+
+    private int bodyIndex;
+    private int eyesIndex;
+	// Start is called before the first frame update
+	void Start()
+    {
+        Material[] matArray = gameObject.GetComponent<SkinnedMeshRenderer>().materials;
+        matArray[0] = bodyColor[PlayerPrefs.GetInt("bodyColor")];
+        matArray[1] = eyesColor[PlayerPrefs.GetInt("eyesColor")];
+        gameObject.GetComponent<SkinnedMeshRenderer>().materials = matArray;
+        Debug.Log("EYES" + PlayerPrefs.GetInt("eyesColor"));
+        Debug.Log("BODY" + PlayerPrefs.GetInt("bodyColor"));
+
+    }
+
+
+    // Update is called once per frame
+
+}
+
--- DSP_Project/Assets/Scripts/manageAppearence.cs
+++ DSP_Project/Assets/Scripts/manageAppearence.cs
@@ -18,9 +18,11 @@ public class manageAppearence : MonoBehaviour
 	[SerializeField]
 	private Material[] bodyColor;
 
-	Dictionary<AppearenceDetail, int> chosenAppearence;
-	int indexBody;
-	int indexE;
+
+    Dictionary<AppearenceDetail, int> chosenAppearence;
+    private appearanceManager theManager;
+	int indexBody=0;
+	int indexE=0;
 
 
     public TMP_Dropdown myDropdownEyes, myDropDownBody;
@@ -29,7 +31,22 @@ public class manageAppearence : MonoBehaviour
 
     private void Start()
     {
+       
+        if (!PlayerPrefs.HasKey("bodyColor")&& !PlayerPrefs.HasKey("bodyColor"))
+        {
+            //standard color
+            PlayerPrefs.SetInt("bodyColor", indexBody);
+            PlayerPrefs.SetInt("eyesColor", indexE);
+        }
+        else if(PlayerPrefs.HasKey("bodyColor") && PlayerPrefs.HasKey("bodyColor"))
+        {
 
+            Material[] matArray = colorAnchor.materials;
+            matArray[0] = bodyColor[PlayerPrefs.GetInt("bodyColor")];
+            matArray[1] = eyesColor[PlayerPrefs.GetInt("eyesColor")];
+            colorAnchor.materials = matArray;
+        }
+ 
         myDropdownEyes.AddOptions(eyesColors);
         myDropDownBody.AddOptions(bodyColors);
 
@@ -37,21 +54,39 @@ public class manageAppearence : MonoBehaviour
     public void BodyColorAppearence()
 	{
 		indexBody = myDropDownBody.value;
-		Material[] matArray = colorAnchor.materials;
-		matArray[0] = bodyColor[indexBody];
-		colorAnchor.materials = matArray;
-	}
+		Material[] matBody = colorAnchor.materials;
+		matBody[0] = bodyColor[indexBody];
+		colorAnchor.materials = matBody;
+     
+    }
 	public void EyesColorAppearence()
 	{
 		indexE = myDropdownEyes.value;
-		Material[] matArray = colorAnchor.materials;
-		matArray[1] = eyesColor[indexE];
-		colorAnchor.materials = matArray;
-	}
+		Material[] matEyes = colorAnchor.materials;
+		matEyes[1] = eyesColor[indexE];
+		colorAnchor.materials = matEyes;
+        
+    }
 	public void SaveAppearence()
 	{
 		chosenAppearence = new Dictionary<AppearenceDetail, int>();
 		chosenAppearence.Add(AppearenceDetail.SKIN_COLOR, indexBody);
 		chosenAppearence.Add(AppearenceDetail.EYES_COLOR, indexE);
-	}
+        PlayerPrefs.SetInt("bodyColor", indexBody);
+        Debug.Log("body color set to"+indexBody);
+
+        PlayerPrefs.SetInt("eyesColor", indexE);
+        Debug.Log("eyes color set to" + indexE);
+
+    }
+    public void Revert()
+    {
+        PlayerPrefs.SetInt("bodyColor", 0);
+        PlayerPrefs.SetInt("eyesColor", 0);
+
+        Material[] matArray = colorAnchor.materials;
+        matArray[0] = bodyColor[PlayerPrefs.GetInt("bodyColor")];
+        matArray[1] = eyesColor[PlayerPrefs.GetInt("eyesColor")];
+        colorAnchor.materials = matArray;
+    }
 }
